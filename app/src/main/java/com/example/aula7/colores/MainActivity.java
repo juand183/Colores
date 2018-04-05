f22ca5d10deb2f734393878245e8b4e5fec6ef6a
pc714 <pc714@iucesmag.edu.co>
2018-04-05 13:52:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aula7/colores/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.aula7.colores;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements  SeekBar.OnSeekBarChangeListener {
+    int seekR, seekG, seekB, seekA;
+
+
+   SeekBar seekBarRed, seekBarGreen, seekBarBlue, seekBarAlpha;
+   View viewBackground;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBarRed = (SeekBar) findViewById(R.id.id_sb_red);
+        seekBarGreen = (SeekBar) findViewById(R.id.id_sb_green);
+        seekBarBlue = (SeekBar) findViewById(R.id.id_sb_blue);
+        seekBarAlpha = (SeekBar) findViewById(R.id.id_sb_alpha);
+        viewBackground = (View) findViewById(R.id.id_v_backgraound);
+
+        seekBarRed.setOnSeekBarChangeListener(this);
+        seekBarGreen.setOnSeekBarChangeListener(this);
+        seekBarBlue.setOnSeekBarChangeListener(this);
+        seekBarAlpha.setOnSeekBarChangeListener(this);
+
+
+    }
+    public void updateBackgrpund (){
+        seekR = seekBarRed.getProgress();
+        seekG = seekBarGreen.getProgress();
+        seekB = seekBarBlue.getProgress();
+        seekA = seekBarAlpha.getProgress();
+
+        int color = Color.argb(seekA, seekR, seekG, seekB);
+        viewBackground.setBackgroundColor(color);
+    }
+
+    // aumentar un textview a lado de la barra
+
+    // aumentar en el menu una paleta de colores y cuando le de un clic en alguno cambie
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_index, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_item_menu_1:
+                Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.id_item_menu_2:
+                Toast.makeText(this, "Item 2", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_index, menu);
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        updateBackgrpund();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
+
